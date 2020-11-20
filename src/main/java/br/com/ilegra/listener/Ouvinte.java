619b747bd7a0d8f36eafe5a7a37cc110e373e0d8
Leandro Magalhaes <lcmagalhaes@stefanini.com>
2020-11-20 01:07:21 -0300

    Inclusão do ouvinte e ajuste finais no serviço.

--- src/main/java/br/com/ilegra/listener/Ouvinte.java
+++ src/main/java/br/com/ilegra/listener/Ouvinte.java
@@ -35,26 +35,21 @@ public class Ouvinte implements FileAlterationListener {
 
     @Override
     public void onStart(FileAlterationObserver fileAlterationObserver) {
-
     }
 
     @Override
     public void onDirectoryCreate(File file) {
-
     }
 
     @Override
     public void onDirectoryChange(File file) {
-
     }
 
     @Override
     public void onDirectoryDelete(File file) {
-
     }
 
     @Override
     public void onStop(FileAlterationObserver fileAlterationObserver) {
-
     }
 }
